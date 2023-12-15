bee64f6a094efe82b8658771b9b69b79793ea308
DanielAglr <81238732+DanielAglr@users.noreply.github.com>
2023-12-15 11:03:56 -0800

    Detect recognized faces in real time

--- detectFaces.py
+++ detectFaces.py
@@ -1,44 +1,55 @@
 import cv2
+from keras.models import load_model
 
-recognizer = cv2.face.LBPHFaceRecognizer.create()
-recognizer.read("data\\face_recognizer.yml")
+import pickle
+import numpy as np
 
-face_classifier = cv2.CascadeClassifier(
-  cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
-)
+IMAGE_WIDTH = 244
+IMAGE_HEIGHT = 244
+
+face_cascade = cv2.CascadeClassifier("data\\haarcascade_frontalface_default.xml")
+
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+model = load_model("data\\trained_face_cnn_model.h5")
+
+face_label_file = "data\\faceLabels.pickle"
+with open(face_label_file, "rb") as f: 
+  class_dict = pickle.load(f)
+  labels = {key: value for key, value in class_dict.items()}
+  print(labels)
 
 live_video = cv2.VideoCapture(0)
-
-def detectFace(frame):
-  gray_image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-  faces = face_classifier.detectMultiScale(gray_image, 1.1, 5, minSize=(40, 40))
-  return faces
-
-def recognizeFace(frame):
-  faces = detectFace(frame)
-
-  for (x, y, w, h) in faces:
-    face_roi = frame[y:y + h, x:x + w]
-    face_gray = cv2.cvtColor(face_roi, cv2.COLOR_BGR2GRAY)
-
-    label, confidence = recognizer.predict(face_gray)
-
-    name = chr(label) if confidence > 80 else "Unknown Person"
-    text = f'{name} - {confidence: .2f}%'
-
-    cv2.putText(frame, text, (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 4)
-
 
 while True:
   ret, frame = live_video.read()
 
-  recognizeFace(frame)
+  faces = face_cascade.detectMultiScale(cv2.cvtColor(frame, cv2.COLOR_BGR2RGB), scaleFactor=1.3, minNeighbors=5)
 
-  cv2.imshow('Facial Recognition', frame)
+  for (x, y, w, h) in faces: 
+    roi_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)[y: y + h, x: x + w]
+    
+    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
 
-  ESCAPE_KEY = 27
-  if cv2.waitKey(1) == ESCAPE_KEY:
+    size = (IMAGE_WIDTH, IMAGE_HEIGHT)
+    
+    resized_image = cv2.resize(roi_rgb, (IMAGE_WIDTH, IMAGE_HEIGHT))      
+    
+    image_array = np.array(resized_image, "uint8")
+
+    img = image_array.reshape(1, IMAGE_WIDTH, IMAGE_HEIGHT, 3).astype("float32") / 255.0
+    
+    predicted_prob = model.predict(img)
+
+    name = labels[predicted_prob[0].argmax()]
+
+    cv2.putText(frame, f'({name})', (x, y - 10), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 0, 255), 2, cv2.LINE_AA)
+
+  cv2.imshow("Image", frame)
+  
+  key = cv2.waitKey(1) & 0xFF
+  if key == ord("q"):
     break
 
 live_video.release()
